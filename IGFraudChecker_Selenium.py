94f126e8f425269f0713d59a5cc53f672c4290c8
Drod917 <drodriguez11375@gmail.com>
2020-08-07 16:39:26 -0400

    My last commit introduced a write-out bug... This one fixed it

--- IGFraudChecker_Selenium.py
+++ IGFraudChecker_Selenium.py
@@ -15,7 +15,7 @@ password = getpass(prompt="Enter your password: ")
 
 options = webdriver.FirefoxOptions()
 # Set to true for a live view
-options.headless = False
+options.headless = True
 driver = webdriver.Firefox(options=options)
 wait = WebDriverWait(driver, 10)
 
@@ -139,13 +139,7 @@ followers_found = len(names_list)
 print("End of followers reached")
 print("Found " + str(followers_found) + " followers in " + str(end - start) + "s")
 
-# Dismiss follower popup to prepare for subsequent searches
-dismiss = driver.find_element_by_xpath(
-    '/html/body/div[4]/div/div/div[1]/div/div[2]/button')
-dismiss.click()
-
 # Scroll through the follower list to generate a complete file
-# %%
 with open('names.txt', 'w') as names_file:
     for account in names_list:
         names_file.write(account.text + str('\n'))
@@ -153,11 +147,13 @@ with open('names.txt', 'w') as names_file:
     names_file.close()
 print("File written")
 
+# Dismiss follower popup to prepare for subsequent searches
+dismiss = driver.find_element_by_xpath(
+    '/html/body/div[4]/div/div/div[1]/div/div[2]/button')
+dismiss.click()
+
 # %%
 driver.close()
 # <div class="                    Igw0E     IwRSH        YBx95       _4EzTm                                                                                                               _9qQ0O ZUqME" style="height: 32px; width: 32px;"><svg aria-label="Loading..." class="  By4nA" viewBox="0 0 100 100"><rect fill="#555555" height="6" opacity="0" rx="3" ry="3" transform="rotate(-90 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.08333333333333333" rx="3" ry="3" transform="rotate(-60 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.16666666666666666" rx="3" ry="3" transform="rotate(-30 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.25" rx="3" ry="3" transform="rotate(0 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.3333333333333333" rx="3" ry="3" transform="rotate(30 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.4166666666666667" rx="3" ry="3" transform="rotate(60 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.5" rx="3" ry="3" transform="rotate(90 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.5833333333333334" rx="3" ry="3" transform="rotate(120 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.6666666666666666" rx="3" ry="3" transform="rotate(150 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.75" rx="3" ry="3" transform="rotate(180 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.8333333333333334" rx="3" ry="3" transform="rotate(210 50 50)" width="25" x="72" y="47"></rect><rect fill="#555555" height="6" opacity="0.9166666666666666" rx="3" ry="3" transform="rotate(240 50 50)" width="25" x="72" y="47"></rect></svg></div>
 # <svg class=" By4nA" aria-label="Loading..." viewBox="0 0 100 100" /> IS THE LOADING SYMBOL WHEN SCROLLING DOWN
 
-
-
-# %%
