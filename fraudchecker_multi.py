4f079910e724a46999d2348a9b91dd2f3708d9ee
Drod917 <drodriguez11375@gmail.com>
2020-08-24 23:05:52 -0400

    FIXED multiprocessing finally. Multithreading file IO is not the move on python.

--- fraudchecker_multi.py
+++ fraudchecker_multi.py
@@ -1,7 +1,8 @@
 from instaloader import instaloader, Profile
 from distribution import Distribution
 from instaloader.exceptions import ProfileNotExistsException, ConnectionException
-import threading
+import poolworker
+import multiprocessing
 import numpy as np 
 import concurrent.futures
 import pandas as pd
@@ -16,22 +17,20 @@ class FraudChecker():
         self.loader = instaloader.Instaloader() 
         self.username = username 
         self.password = password
-        self.lock = threading.Lock()
         self.fraud_target_data = []
 
-    def target(self, fraud_user: str):
+    def target(self, fraud_target: str):
         self.loader = loader = instaloader.Instaloader() # reload to avoid Profile.from_username bug
-        self.fraud_target_username = fraud_user
         try:
-            self.fraud_target = Profile.from_username(loader.context, fraud_user)
-            print('Target found: ', fraud_user)
+            self.fraud_target = Profile.from_username(loader.context, fraud_target)
+            print('Target found: ', fraud_target)
         except:
-            print('Failed to acquire profile %s', fraud_user)
+            print('Failed to acquire profile %s', fraud_target)
             pass
     
     def check_for_fraud(self):
         filename = self.fraud_target.username + '_followers.txt'
-        print("Loading followers...")
+        print("Loading " + self.fraud_target.username + '\'s followers...')
         followers = []
         # Look for first checkpoint
         if os.path.exists(filename):
@@ -64,101 +63,56 @@ class FraudChecker():
     def __get_metrics(self) -> pd.DataFrame:
         # TODO: Each pool worker waits for the lock,
         # appending each follower it retrieves to the list
-        def pool_worker(pool: [], worker_idx):
-            pool_loader = instaloader.Instaloader()
-            filename = self.fraud_target_username + '_build_file_' + str(worker_idx)
-            build_file = []
-            
-            # Try to open build file
-            if os.path.exists(filename):
-                build_file = open(filename, 'r', newline='')
-                last_loaded_follower = 0
-                for line in build_file.readlines():
-                    last_loaded_follower += 1
-                build_file.close()
-                build_file = open(filename, 'a', newline='')
-                print("Continuing from line " + str(last_loaded_follower), flush=True)
-                followers = pool[last_loaded_follower:]
-            else:
-                build_file = open(filename, 'w', newline='')
-                followers = pool
-            csv_writer = csv.writer(build_file)
-
-            # Continue loading from checkpoint
-            try:
-                for user in tqdm(pool):
-                    new_user = grab_follower_metadata(pool_loader, user)
-                    csv_writer.writerow(new_user) # write to checkpoint
-            except KeyboardInterrupt:
-                print("\nKeyboard interrupt detected, exiting...")
-                build_file.close()
-                print("Build file written to " + filename)
-                sys.exit()
-            except ConnectionException:
-                print("429 Too many requests: redirected at login")
-                build_file.close()
-                print("Build file written to " + filename)
-                return
-            build_file.close()
-            print("Build file written to " + filename)
-
-        def grab_follower_metadata(loader, user: str) -> []:
-            try:
-                profile = Profile.from_username(loader.context, user)
-            except ProfileNotExistsException:
-                print("Follower " + user + " not found! Logging in to retry...")
-                loader.login(self.username, self.password)
-                try:
-                    profile = Profile.from_username(loader.context, user)
-                except:
-                    print("Follower " + user + " not found ! Skipping...")
-                    loader = instaloader.Instaloader()
-                    return
-                loader = instaloader.Instaloader() # logout
-            ret_user = [user, profile.followers, profile.followees]
-            return ret_user
-
         print("Pulling follower metadata...")
         followers = self.followers
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as exe:
-            # Isolate & multithread this
-            size = len(followers)
-            q1 = followers[:(size//4)]                     #  0% - 25%
-            q2 = followers[size//4:(size//2)]           # 26% - 50%
-            q3 = followers[size//2:(size - size//4)]    # 51% - 75%
-            q4 = followers[size - size//4:size]      # 76% - 100%            
-            exe.submit(pool_worker(q1, 1))
-            exe.submit(pool_worker(q2, 2))
-            exe.submit(pool_worker(q3, 3))
-            exe.submit(pool_worker(q4, 4))
-            #exe.submit(pool_worker(followers, 1))
+        pool = multiprocessing.Pool(processes=4)
+        size = len(followers)
+        q1 = followers[:(size//4)]                     #  0% - 25%
+        q2 = followers[size//4:(size//2)]           # 26% - 50%
+        q3 = followers[size//2:(size - size//4)]    # 51% - 75%
+        q4 = followers[size - size//4:size]      # 76% - 100%  
+        payload = [self.fraud_target.username, self.username, self.password]
+        results = pool.starmap(poolworker.worker, [(q1,1,payload),(q2,2,payload),(q3,3,payload),(q4,4,payload)])
 
     # Populate 'build_file' with the metadata from each follower in
     # the fraud target's follower list.
     # WARNING: DO NOT USE UNLESS THE BUILD FILE IS COMPLETE
     def build_dataframe(self):
-        build_filename = self.fraud_target_username + '_build_file.csv'
+        build_file_1 = self.fraud_target.username + '_build_file_1'
+        build_file_2 = self.fraud_target.username + '_build_file_2'
+        build_file_3 = self.fraud_target.username + '_build_file_3'
+        build_file_4 = self.fraud_target.username + '_build_file_4'
         # Read the build_file into a dataframe with column labels
         try:
-            df = pd.read_csv(build_filename, header=None, names=['username','followers','following'])
+            df_1 = pd.read_csv(build_file_1, header=None, names=['username','followers','following'])
+            df_2 = pd.read_csv(build_file_2, header=None, names=['username','followers','following'])
+            df_3 = pd.read_csv(build_file_3, header=None, names=['username','followers','following'])
+            df_4 = pd.read_csv(build_file_4, header=None, names=['username','followers','following'])
+            df = df_1.append(df_2.append(df_3.append(df_4)))
         except FileNotFoundError as e:
             print("No build file found. DataFrame build failed")
             return
-        self.fraud_target_data = df
-        df_filename = self.fraud_target_username + '_dataframe.csv'
+        df_filename = self.fraud_target.username + '_dataframe.csv'
         df.to_csv(df_filename, index=False)
-        self.filename = df_filename
+        self.df_filename = df_filename
         self.loader = instaloader.Instaloader()
         print("Dataframe written to " + df_filename)
         # TODO: Add safety before removing.
         # Maybe: Compare the length of the build file with the length
         # of the follower list before removing the build file.
         # Then, I can remove the follower file as well.
-        os.remove(build_filename)
+        #os.remove(build_filename)
         return df 
 
     def show_distribution(self):
-        dist = Distribution(self.filename, self.fraud_target_username)
+        dist = Distribution(self.df_filename)
         dist.get_distribution()
 
 
+if __name__ == '__main__':
+    print("Main thread")
+    bot = FraudChecker('_danielchristopher','wgHnvRQi11951!!%')
+    bot.target('arodriguez4ny')
+    bot.check_for_fraud()
+    bot.build_dataframe()
+    bot.show_distribution()
